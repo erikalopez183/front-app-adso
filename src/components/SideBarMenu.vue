35dded2a76d215eb56963147e95843062e335a8f
Erika <erikalopez183@gmail.com>
2024-11-04 14:49:03 -0500

    se crearon las rutas y las vistas del proyecto

---
+++ src/components/SideBarMenu.vue
@@ -0,0 +1,52 @@
+<template>
+
+        <el-menu
+        :default-active="$route.path"
+        router
+          class="el-menu-vertical-demo"
+          background-color="#242629"
+          text-color="#fff"
+          collapse-transition
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>Administrador</span>
+            </template>
+  
+            <el-menu-item index="personas">Personas</el-menu-item>
+            <el-menu-item index="cargos">Cargo</el-menu-item>
+            <el-menu-item index="Pais">Pais</el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>Configuracion</span>
+            </template>
+  
+            <el-menu-item index="1-2">Personas</el-menu-item>
+            <el-menu-item index="1-3">Cargo</el-menu-item>
+            <el-menu-item index="pais">Pais</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+  </template>
+  
+  <script lang="ts" setup>
+  import {Location} from '@element-plus/icons-vue';
+  </script>
+  
+  <style scoped>
+  html, body {
+    margin: 0;
+    padding: 0;
+    /* height: 100%; */
+  }
+  
+
+  
+  .el-menu-vertical-demo {
+    width: 250px;
+    min-height: 100%;
+  }
+  </style>
+  
